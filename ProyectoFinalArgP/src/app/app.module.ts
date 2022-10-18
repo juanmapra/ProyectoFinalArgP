8b59c7c0b08de9c98cb20630c62c3fec9bc7c17a
Manna97 <juanmapra@yahoo.com.ar>
2022-10-18 05:03:05 -0300

    Try

---
+++ ProyectoFinalArgP/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { SocialsComponent } from './components/socials/socials.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    SocialsComponent,
+    BannerComponent,
+    AboutmeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ProyectsComponent,
+    SkillsComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
